e5247c96267ecfbe52f83c8b29b9de7c09663924
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-06-20 11:16:27 +0100

    Created newsite

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Jumbotron } from 'reactstrap';
+import './App.css';
+import SiteNav from './Components/SiteNav';
+
+export default function App() {
+    return (
+        <div>
+            <SiteNav />
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <Container>
+                <Jumbotron>
+                    <h1 className="display-3">Hello, world!</h1>
+                    <p className="lead">My website is going through a small re-design...</p>
+                    <hr className="my-2" />
+                    <p>If you wish to have a look at my current projects, my Github and roller Derby Officials CV links can be found in the navbar above</p>
+                </Jumbotron>
+            </Container>
+        </div>
+    );
+}
---
+++ src/Components/SiteNav.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+  } from 'reactstrap';
+
+export default function SiteNav()
+{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+            <Navbar color="dark" dark expand="md">
+            <Container>
+                <NavbarBrand href="/">queekus.com</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="https://docs.google.com/spreadsheets/d/1j_j66dSuO2WUfHwfGYWleSU1uLlmhVzJEHJTVEzrvWo">Roller Derby Officials CV</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/queekusme">GitHub</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Container>
+        </Navbar>
+    );
+}
