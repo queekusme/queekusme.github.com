415a4f28c259d3f317f0eda30d56d9e313bf86df
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-06-23 20:27:39 +0100

    Credit bootswatch

--- src/Sections/Footer.tsx
+++ src/Sections/Footer.tsx
@@ -9,7 +9,8 @@ export default function FeaturedProjects()
                 Site designed by Annabelle Kennedy using&nbsp;
                 <a href="https://reactjs.org/">React</a>,&nbsp;
                 <a href="https://reactstrap.github.io/">Reactstrap</a> and&nbsp;
-                <a href="https://getbootstrap.com">Bootstrap 4</a>.
+                <a href="https://getbootstrap.com">Bootstrap 4</a> with the &nbsp;
+                <a href="https://bootswatch.com/superhero/">Bootswatch Superhero</a> stylesheet.
                 &copy; Annabelle Kennedy (2021)
             </span>
             <p/>
